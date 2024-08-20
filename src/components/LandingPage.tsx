d874283840df5746d7b9e2140276fcbb74940b4e
Ammar Ahmed <ammar7298@gmail.com>
2024-08-20 12:45:35 +0500

    chnag

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from "next/image";
+import Image from 'next/image';
 import './LandingPage1.css';
 
 const LandingPage = () => {
@@ -12,24 +12,26 @@ const LandingPage = () => {
         <div className="menu">
           <i className="fa fa-bars" aria-hidden="true"></i>
         </div>
-        {/* <nav className="navbar">
-          <a href="#Home">Home</a>
-        </nav> */}
+        <nav className="navbar">
+          {/* <a href="#Home">Home</a> */}
+          {/* Add more navigation links here if needed */}
+        </nav>
       </header>
-      
+
       <section className="home" id="Home">
         <div className="content">
           <h1>
             I am <span>Romeo GFX</span>
           </h1>
-          {/* <h3 className="text-animation">
-            I specialize in <span> Animations</span>
-          </h3> */}
-          <p >
-          I am Romeo GFX, a passionate and dedicated graphic designer from Pakistan with over three years of experience in the field. My expertise lies in creating compelling YouTube thumbnails that not only capture attention but also encourage viewers to engage with the content. Whether you are a YouTuber or a business owner, my goal is to elevate your visual presence and bring your ideas to life with creative and impactful designs.
+          <div className="text-animation">
+            <span>Animations</span>
+            <span>Graphics Designing</span>
+          </div>
+          <p>
+            I am Romeo GFX, a passionate and dedicated graphic designer from Pakistan with over three years of experience in the field. My expertise lies in creating compelling YouTube thumbnails that not only capture attention but also encourage viewers to engage with the content. Whether you are a YouTuber or a business owner, my goal is to elevate your visual presence and bring your ideas to life with creative and impactful designs.
           </p>
           <div className="icons">
-            <a href="https://www.instagram.com/romeo_gfx_/">
+            <a href="https://www.instagram.com/romeo_gfx_/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-instagram" aria-hidden="true"></i>
             </a>
           </div>
