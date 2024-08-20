e0f9a8a75e49c83f8f3cc68dcae0a0f4631d975a
Ammar Ahmed <ammar7298@gmail.com>
2024-08-20 12:50:31 +0500

    hell

--- src/components/Cards3.tsx
+++ src/components/Cards3.tsx
@@ -7,7 +7,7 @@ import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 const Cards3 = () => {
   return (
     <section className="p-6 bg-black">
-      <h2 className="text-3xl font-bold text-white text-center mb-8">
+      <h2 className="text-7xl font-bold text-white text-center mb-8">
         My <span className="text-red-600">Projects</span>
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
