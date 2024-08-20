09913cfe3439f6bf7106d255939ac66b52aad716
Ammar Ahmed <ammar7298@gmail.com>
2024-08-20 12:31:35 +0500

    changes

--- src/components/Cards3.tsx
+++ src/components/Cards3.tsx
@@ -7,7 +7,9 @@ import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 const Cards3 = () => {
   return (
     <section className="p-6 bg-black">
-      <h2 className="text-3xl font-bold text-white text-center mb-8">My Projects</h2>
+      <h2 className="text-3xl font-bold text-white text-center mb-8">
+        My <span className="text-red-600">Projects</span>
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {[...Array(6)].map((_, index) => (
           <CardContainer key={index} className="inter-var">
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -22,10 +22,10 @@ const LandingPage = () => {
           <h1>
             I am <span>Romeo GFX</span>
           </h1>
-          <h3 className="text-animation">
-            I specialize in <span></span>
-          </h3>
-          <p>
+          {/* <h3 className="text-animation">
+            I specialize in <span> Animations</span>
+          </h3> */}
+          <p >
           I am Romeo GFX, a passionate and dedicated graphic designer from Pakistan with over three years of experience in the field. My expertise lies in creating compelling YouTube thumbnails that not only capture attention but also encourage viewers to engage with the content. Whether you are a YouTuber or a business owner, my goal is to elevate your visual presence and bring your ideas to life with creative and impactful designs.
           </p>
           <div className="icons">
