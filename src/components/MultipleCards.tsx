8814814be969cc316a8e91b7568869b9b35c69d7
Ammar Ahmed <ammar7298@gmail.com>
2024-08-22 09:34:51 +0500

    all

--- src/components/MultipleCards.tsx
+++ src/components/MultipleCards.tsx
@@ -12,7 +12,7 @@ const MultipleCards = () => {
       <section className="w-full h-full py-20">
       <div className="max-w-7xl mx-auto px-4">
           <h2 className="text-xl md:text-5xl font-bold text-white font-sans mb-8">
-          Explore My Media Collection.
+          Explore My Media Collection
           </h2>
           <Carousel items={cards} />
       </div>
@@ -25,10 +25,9 @@ const DummyContent1 = () => {
         <div className="bg-black  p-8 md:p-14 rounded-3xl mb-4">
             <p className="text-white text-base md:text-2xl font-sans max-w-3xl mx-auto">
                 <span className="font-bold text-red-600">
-                    Discover the power of AI.
+                    Discover the power of AI
                 </span>{" "}
-                AI is revolutionizing the way we interact with technology. From personalized recommendations to intelligent automation, AI is here to enhance every aspect of your life.
-            </p>
+                AI is revolutionizing the way we interact   </p>
             <div className="flex gap-2  my-2 flex-col">
                 <Image
                     src="/imgprofile.png"
@@ -54,10 +53,9 @@ const DummyContent2 = () => {
         <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
             <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
                 <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                    Boost your productivity with new tools.
+                    Boost your productivity with new tools
                 </span>{" "}
-                Explore our latest productivity tools designed to help you manage your time better, collaborate efficiently, and achieve more.
-            </p>
+                AI is revolutionizing the way we interact </p>
             <div className="m-2">
                 <Image
                     src="/imgproductivity.png"
@@ -76,10 +74,9 @@ const DummyContent3 = () => {
         <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
             <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
                 <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                    Apple Vision Pro: The future is here.
+                    Apple Vision Pro: The future is here
                 </span>{" "}
-                Experience the latest in augmented reality with the Apple Vision Pro. It's not just a product; it's a portal to new dimensions.
-            </p>
+                AI is revolutionizing the way we interact  </p>
             <div className="m-2">
                 <Image
                     src="/imgvisionpro.png"
@@ -98,10 +95,9 @@ const DummyContent4 = () => {
         <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
             <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
                 <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                    Navigate your world with iPhone 15 Pro Max.
+                    Navigate your world with iPhone 15 Pro Max
                 </span>{" "}
-                The new Maps feature on the iPhone 15 Pro Max brings you the most accurate and detailed maps yet. Explore the world with confidence.
-            </p>
+                AI is revolutionizing the way we interact   </p>
             <div className="m-2">
                 <Image
                     src="/imgmaps.png"
@@ -124,19 +120,19 @@ const data = [
     },
     {
         category: "Productivity",
-        title: "Enhance your productivity.",
+        title: "Click to see more",
         src: "/img1.png",
         content: <DummyContent2 />,
     },
     {
         category: "Product",
-        title: "Launching the new Apple Vision Pro.",
+        title: "Click to see more",
         src: "/img1.png",
         content: <DummyContent3 />,
     },
     {
         category: "Product",
-        title: "Maps for your iPhone 15 Pro Max.",
+        title: "Click to see more",
         src: "/img1.png",
         content: <DummyContent4 />,
     },
