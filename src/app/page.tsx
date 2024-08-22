52a0d092e8a7a074ebdc5a5cbb3beabae18e3808
Ammar Ahmed <ammar7298@gmail.com>
2024-08-22 09:22:42 +0500

    new cars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Cards3 from "@/components/Cards3";
 import Landing from "@/components/Landing";
+import MultipleCards from "@/components/MultipleCards";
 
 export default function Home() {
   return (
     <>
-    <Landing/>
-    <Cards3/>
+     <Landing/>
+    <MultipleCards/>
+     <Cards3/>
     </>
   );
 }
