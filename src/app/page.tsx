259ebd8fbc570c693be2d61195ac5cdd3ebd840d
Ammar Ahmed <ammar7298@gmail.com>
2024-08-23 12:58:56 +0500

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Cards3 from "@/components/Cards3";
 import Landing from "@/components/Landing";
-import LandingNew from "@/components/LandingNew";
 import MultipleCards from "@/components/MultipleCards";
 import PricingPlan from "@/components/PricingPlan";
 import PricingSection from "@/components/PricingSection";
