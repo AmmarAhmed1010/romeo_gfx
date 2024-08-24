a6be42fea261157bd41ea2e973317bd837ffee0c
Ammar Ahmed <ammar7298@gmail.com>
2024-08-24 16:26:24 +0500

    A

--- src/components/GenerateText.tsx
+++ src/components/GenerateText.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
  
-const words = `I am Romeo GFX, a dedicated graphic designer from Pakistan with over three years of experience. I specialize in creating eye-catching YouTube thumbnails that engage viewers. My goal is to enhance your visual presence with creative and impactful designs.`;
+const words = `I am Romeo GFX a dedicated graphic designer from Pakistan with over three years of experience I specialize in creating eye-catching YouTube thumbnails that engage viewers My goal is to enhance your visual presence with creative and impactful designs`;
  
 const GenerateText = () => {
     return <TextGenerateEffect words={words} />;
