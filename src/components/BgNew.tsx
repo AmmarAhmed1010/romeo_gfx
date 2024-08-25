0594f98e996e55ac6f777fd8e64747508c9d4586
Ammar Ahmed <ammar7298@gmail.com>
2024-08-25 13:08:05 +0500

    d

--- src/components/BgNew.tsx
+++ src/components/BgNew.tsx
@@ -17,7 +17,7 @@ const BgNew = () => {
     <h1>RomeoGFX</h1>
    </div>
    {/* Main */}
-   <div className="w-full  h-screen flex flex-col-reverse">
+   <div className="w-full h-screen flex flex-col-reverse">
      {/* DivLeft */}
     <div className="h-full bg-yellow-200 flex justify-center px-8 py-2 text-white">
     <p>
@@ -26,11 +26,11 @@ Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem eum voluptatem 
     </p>
     </div>
     {/* DivRight */}
-    <div className="h-full flex justify-center bg-red-200 items-center">
+    <div className="w-full flex justify-center py-2 bg-red-200">
       <Image
               src="/imgprofile.png"
               alt="Profile Image"
-              className="rounded-[50%] shadow-[0_0_10px_2px_rgba(255,0,0,0.6)] hover:shadow-[0_0_20px_10px_rgba(255,0,0,0.8)] transition-shadow duration-500"
+              className="rounded-[50%] shadow-[0_0_10px_2px_rgba(255,0,0,0.6)]"
               width={300} // Adjusted size for better view on smaller screens
               height={300}
             />
