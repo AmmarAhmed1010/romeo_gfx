38c5962de09dec2740e56b82f89bf553367542ff
Ammar Ahmed <ammar7298@gmail.com>
2024-10-25 11:40:34 +0500

    thumbnial 4 i premium

--- src/components/CardsDesign.tsx
+++ src/components/CardsDesign.tsx
@@ -21,7 +21,7 @@ const CardsDesign = () => {
     {
       title: 'Premium Plan',
       price: '3599',
-      features: ['3 HD Thumbnail', '1 Logo', '1 Banner', '1 Professional Overlay'],
+      features: ['4 HD Thumbnail', '1 Logo', '1 Banner', '1 Professional Overlay'],
     },
   ];
 
